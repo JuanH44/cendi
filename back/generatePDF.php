0a40d2a3996bd856524b769a2e90dcfff6a640c0
Juan Hdez <juanh7522@gmail.com>
2022-05-24 02:48:58 -0500

    pdf configurado para UTF-8

--- back/generatePDF.php
+++ back/generatePDF.php
@@ -1,58 +1,41 @@
-<?php 
-    //require("fpdf184/fpdf.php");
+<?php
+    /* Clase fpdf */
+    require("fpdf184/fpdf.php");
     require("tfpdf/tfpdf.php");
 
-    //define('FPDF_FONTPATH',"fpdf184/fontType/");
-  //  require("fpdf184/makefont/makefont.php");
-   // MakeFont('fpdf184/fontType/Arial.ttf','cp1250');
-
-
-   
-   
     class PDF extends tFPDF
     {
-    // Cabecera de página
-    function Header()
-    {
-        $this->SetFont('Arial','B',15);
-        // Move to the right
-        $this->Cell(80);
-        // Title
-        $this->Cell(30,10,'Title',1,0,'C');
-        // Line break
-        $this->Ln(20);
-        // Logo
-    //    $this->Image('./encabezado.fw.png',10,8,200);
-      //  $this->Ln(20);
-    }
+        // Cabecera de página
+        function Header()
+        {
+            // Logo
+            $this->Image('./encabezado.fw.png',10,8,200);
+            $this->Ln(20);
+        }
 
-    // Pie de página
+        // Pie de página
         function Footer()
         {
             // Posición: a 1.5 cm del final
-            $this->SetY(-30);
+            $this->SetY(-15);
             // Arial italic 8
-            $pdf->SetFont($fontName,'',12);
+            $this->SetFont('Arial','',8);
             // Número de página
-            $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
-        //    $pdf ->Cell(0,10, 'Formulario de inscripción',1, 1 , 'C');
-           
+            $this->Cell(0,10,'Página '.$this->PageNo().'/{nb}',0,0,'C');
         }
     }
 
-    $pdf = new tFPDF();
-    $pdf -> AddPage();
-    $fontName = 'Arial';
-    $pdf->AddFont($fontName,'','arial.ttf',true);
-   // $pdf->AddFont($fontName,'B','arial.ttf',true);
-    $pdf->SetFont($fontName,'',12);
- //   $pdf->SetFont('Helvetica','',12);
-    
-    
-  
-    $pdf ->Cell(0,10, 'Formulario de inscripción',1, 1 , 'C');
+    $pdf = new PDF();
     $pdf->AliasNbPages();
-    
-    $pdf ->Output();
+    $pdf->AddPage();
+    $pdf->AddFont('Arial','','arial.ttf',true);
+    $pdf->SetFont('Arial','',12);
+
+    $pdf->Cell(0,10,"Formulario de Inscripción",0,1,"C");
+
+    for($i=1;$i<=40;$i++){
+        $pdf->Cell(0,10,'Renglón: D'.$i,0,1);
+    }
+    $pdf->Output();
 
 ?>
