205e30808e0817b0b5e68cb192c35d38bd1b33c7
Jose Angel Espinosa <j.a.e.b.1999a@gmail.com>
2022-06-07 00:45:11 -0500

    El formulario de registro ya tiene todos los campos, envio de JSON de prueba al php y recepcion de JSON en php

--- back/hola.php
+++ back/hola.php
@@ -1,9 +1,24 @@
 <?php 
     require("fpdf184/fpdf.php");
 
+    
+
+    $alumno = $_REQUEST['alumno'];
+    $derecho = $_REQUEST['derechoHab'];
+    $conyuge = $_REQUEST['conyuge'];
+
+    $curp = $alumno['curp'];
+    $folio = $alumno['folio'];
+    echo "{curp: $curp}";
+
+    
+
     $pdf = new FPDF();
     $pdf -> AddPage();
     $pdf ->SetFont('helvetica','B',20);
-    $pdf ->Cell(0,20, 'Hola grupo 4CM4!',1);
+    $pdf ->Cell(0,12, 'Hola grupo 4CM4!',1);
+    $pdf -> AddPage();
+    $pdf ->Cell(10,20,("curp: $curp"),70);
+    $pdf ->Cell(20,50,("folio: $folio"),110);
     $pdf ->Output();
 ?>
