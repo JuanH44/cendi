fa1b53a5db7b68637de108ec7049bd472692738e
Juanh44 <juah7522@gmail.com>
2022-06-12 18:43:34 -0500

    Email estilizado

--- back/mail/sendMail.php
+++ back/mail/sendMail.php
@@ -10,38 +10,46 @@ include("../pdf/stringPDF.php");
 
 $email_user = "equipo3tecweb@gmail.com"; //OJO. Debes actualizar esta línea con tu información
 $email_password = "otlheyotnscyleep"; //OJO. Debes actualizar esta línea con tu información
-$the_subject = "Prueba de PHPMailer ".date("d-m-Y:H:i:s");
+$the_subject = "Registro CENDI";
 $address_to = "juanh7522@gmail.com"; //OJO. Debes actualizar esta línea con tu información
-$from_name = "Tec Web E3";
-$phpmailer = new PHPMailer();
+$from_name = "CENDI";
+$mail = new PHPMailer();
 
 // ---------- datos de la cuenta de Gmail -------------------------------
-$phpmailer->Username = $email_user;
-$phpmailer->Password = $email_password; 
+$mail->Username = $email_user;
+$mail->Password = $email_password; 
 //-----------------------------------------------------------------------
-// $phpmailer->SMTPDebug = 1;
-$phpmailer->SMTPSecure = 'ssl';
-$phpmailer->Host = "smtp.gmail.com"; // GMail
-$phpmailer->Port = 465;
-$phpmailer->IsSMTP(); // use SMTP
-$phpmailer->SMTPAuth = true;
+// $mail->SMTPDebug = 1;
+$mail->SMTPSecure = 'ssl';
+$mail->Host = "smtp.gmail.com"; // GMail
+$mail->Port = 465;
+$mail->IsSMTP(); // use SMTP
+$mail->SMTPAuth = true;
 
-$phpmailer->setFrom($phpmailer->Username,$from_name);
-$phpmailer->AddAddress($address_to); // recipients email
+$mail->setFrom($mail->Username,$from_name);
+$mail->AddAddress($address_to); // recipients email
 
-$phpmailer->Subject = $the_subject;	
-$phpmailer->Body .="<h1 style='color:#3498db;'>Prueba del equipo 3</h1>";
-$phpmailer->Body .= "<p>Esta es una prueba del envío de correo :D</p>";
+//Datos personales--------------------------------------------------------
+  $nombre = "Juan";
+  $folio = "12345";
 
-//$phpmailer->AddAttachment("", "Importante - ".date("d-m-Y"));
 
-global $pdfDoc;
+//------------------------------------------------------------
+$mail->Subject = $the_subject;
+$mail->AddEmbeddedImage('../assets/banne-cendi-min.jpg', 'banner');
 
-$phpmailer->AddStringAttachment($pdfDoc, 'Ficha de Registro.pdf', $encoding = 'base64', $type = 'application/pdf');
 
-$phpmailer->IsHTML(true);
-if(!$phpmailer->Send()) {
-    $message =  "El correo no se ha enviado. Mailer Error: " . $phpmailer->ErrorInfo;
+$mail->Body ='<img alt="Banner del cendi" src="cid:banner" style="width:90%">';
+$mail->Body .="<h1 style='color:#3498db;'>Buen día, $nombre </h1>";
+$mail->Body .= "<p>Usted ha concluido su registro exitosamente. A continuación adjutamos un archivo pdf con los datos registrados. Si usted no ha solicitado el registro, haga caso omiso a este correo.</p>";
+
+global $pdfDoc;$mail->Body .= "<p>Este correo es sólamente informativo, favor de no responer.</p>";
+
+$mail->AddStringAttachment($pdfDoc, "Ficha de Registro $folio.pdf", $encoding = 'base64', $type = 'application/pdf');
+
+$mail->IsHTML(true);
+if(!$mail->Send()) {
+    $message =  "El correo no se ha enviado. Mailer Error: " . $mail->ErrorInfo;
   } else {
     $message = "El correo se ha enviado correctamente";
   }
