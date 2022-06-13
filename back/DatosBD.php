2a6668a05e364a29e931d68b318a72b1280ef06b
TorresAbonceLuis <ltorresa1701@alumno.ipn.mx>
2022-06-13 01:29:51 -0500

    generacion json terminada

--- back/DatosBD.php
+++ back/DatosBD.php
@@ -5,30 +5,23 @@
 
     //datos generales
     $result=array();
+    //declarar consulta
+    $sqlGeneral="
+    select DISTINCT * 
+    from datos_generales as dg 
+    inner join datos_niño as dn on dn.Folio = dg.Folio 
+    inner join datos_derecho as dd on dd.Folio = dg.Folio 
+    RIGHT join conyuge as dc on dc.Folio = dg.Folio UNION 
+    select DISTINCT * 
+    from datos_generales as dg 
+    inner join datos_niño as dn on dn.Folio = dg.Folio 
+    inner join datos_derecho as dd on dd.Folio = dg.Folio 
+    LEFT join conyuge as dc on dc.Folio = dg.Folio";
 
-    $sqlGeneral="select * from datos_generales";//declarar consulta
     $respGen=mysqli_query($conexion,$sqlGeneral);//hacer consulta
-
-    $sqlNin="select * from datos_niño";//declarar consulta
-    $respNin=mysqli_query($conexion,$sqlNin);//hacer consulta
-
-    $sqlDerecho="select * from datos_derecho";//declarar consulta
-    $respDer=mysqli_query($conexion,$sqlDerecho);//hacer consulta
-
-    $sqlConyuge="select * from conyuge";//declarar consulta
-    $respCon=mysqli_query($conexion,$sqlConyuge);//hacer consulta
-
     while($filaGen=mysqli_fetch_assoc($respGen)){
         $result[]=$filaGen;
-    }
-    while($filaNin=mysqli_fetch_assoc($respNin)){
-        $result[]=$filaNin;
-    }
-    while($filaDer=mysqli_fetch_assoc($respDer)){
-        $result[]=$filaDer;
-    }
-    while($filaCon=mysqli_fetch_assoc($respCon)){
-        $result[]=$filaCon;
+        echo " ".$filaGen['Folio'];
     }
     $resultado=json_encode($result);
     file_put_contents("BD.json",$resultado);
