2947d35eef70d946347d5b05d3f21b140cb4550f
TorresAbonceLuis <ltorresa1701@alumno.ipn.mx>
2022-06-13 02:09:36 -0500

    ahora si front actualizado

--- front/js/admin.js
+++ front/js/admin.js
@@ -15,36 +15,47 @@ $(document).ready(function () {
     function construye() {
         let nuevaLista = "<ul class='collection' id='alumnos'> ";
         for (let index = 0; index < alumnos.length; index++) {
-            let nuevoAlumno = "";
-            nuevoAlumno += ("<li class='collection-item avatar' id='usr" + index + "'>");
-            nuevoAlumno += ("    <img src='" + alumnos[index].foto + "' alt='" + alumnos[index].folio + "' class='circle'>");
-            nuevoAlumno += ("    <span class='title'>" + alumnos[index].folio + "</span>");
-            nuevoAlumno += ("    <p class='parrafo'>" + alumnos[index].curp + " <br>");
-            nuevoAlumno += ("        " + alumnos[index].nombre + "");
-            nuevoAlumno += ("    </p>");
-            nuevoAlumno += ("    <a href='#!' class='secondary-content'><i class='material-icons'>grade</i></a>");
-            nuevoAlumno += ("    <div class='row' id='alumno" + index + "'>");
-            nuevoAlumno += ("        <a class='waves-effect waves-light btn ver-mas'>Ver mas</a>");
-            nuevoAlumno += ("        <a class='waves-effect waves-light btn ver-menos'>Ver menos</a>");
-            nuevoAlumno += ("        <a class='waves-effect waves-light btn editar'>Editar</a>");
-            nuevoAlumno += ("        <a class='waves-effect waves-light btn cancelar-edicion'>Cancelar edicion</a>");
-            nuevoAlumno += ("        <a class='waves-effect waves-light btn btn-reset'>Restablecer valores</a>");
-            nuevoAlumno += ("        <a class='waves-effect waves-light right btn red elimina'>Eliminar</a>");
-            nuevoAlumno += ("        <form id='formula" + index + "' class='formula' action='../back/api.php' method='get'>");
-            nuevoAlumno += ("            <!-- Aqui se inserta solito -->");
-            nuevoAlumno += ("        </form>");
-            nuevoAlumno += ("    </div>");
-            nuevoAlumno += ("</li>");
-            nuevaLista += nuevoAlumno;
-        }
+            let nuevaLista = "<ul class='collection' id='alumnos'> ";
+            for (let index = 0; index < alumnos.length; index++) {
+                let nuevoAlumno = "";
+                nuevoAlumno += ("<div id='clave'>");
+                nuevoAlumno += ("<fieldset>");
+                nuevoAlumno += ("<li class='collection-item avatar' id='usr" + index + "'>");
+                nuevoAlumno += ("<div>");
+                nuevoAlumno += ("    <img id='imagen-ipn-2' src='"+alumnos[index].foto+"' alt='" + alumnos[index].folio + "' class='circle'>");
+                nuevoAlumno += ("</div>");
+                nuevoAlumno += ("<div id='letras'>");
+                nuevoAlumno += ("    <span class='title'>" + alumnos[index].folio + "</span>");
+                nuevoAlumno += ("    <p class='parrafo'>" + alumnos[index].curp + " <br>");
+                nuevoAlumno += ("        " + alumnos[index].nombre + "");
+                nuevoAlumno += ("    </p>");
+                nuevoAlumno += ("</div>");
+                nuevoAlumno += ("    <a href='#!' class='secondary-content'><i class='material-icons'>grade</i></a>");
+                nuevoAlumno += ("    <div class='row' id='alumno" + index + "'>");
+                nuevoAlumno += ("        <a id='vermas' class='waves-effect waves-light btn ver-mas'>Ver mas</a>");
+                nuevoAlumno += ("        <a class='waves-effect waves-light btn ver-menos'>Ver menos</a>");
+                nuevoAlumno += ("        <a class='waves-effect waves-light btn editar'>Editar</a>");
+                nuevoAlumno += ("        <a class='waves-effect waves-light btn cancelar-edicion'>Cancelar edicion</a>");
+                nuevoAlumno += ("        <a class='waves-effect waves-light btn btn-reset'>Restablecer valores</a>");
+                nuevoAlumno += ("        <a class='waves-effect waves-light right btn red elimina'>Eliminar</a>");
+                nuevoAlumno += ("        <form id='formula" + index + "' action='../back/api.php' method='get'>");
+                nuevoAlumno += ("            <!-- Aqui se inserta solito -->");
+                nuevoAlumno += ("        </form>");
+                nuevoAlumno += ("    </div>");
+                nuevoAlumno += ("</li>");
+                nuevoAlumno += ("</fieldset>");
+                nuevoAlumno += ("</div>");
+                nuevoAlumno += ("</br>");
+                nuevaLista += nuevoAlumno;
+            
         nuevaLista += "</ul>";
         contenedor.replaceWith(nuevaLista);
         contenedor = $('#alumnos');
 
+        }
+
     }
-
-
-
+}
 
 
     ///Generar a partir de la query al back
